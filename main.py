3326acdf7041459914f0507464e4d1302e3fbcf7
Shannon Komguem <s.komguem@mail.utoronto.ca>
2019-12-30 01:45:17 -0500

    Implemented cloud functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 This will be the file containing my cloud functions.
 """
 from flask import Request
+from model import playlist_data, concert_data
 
 
 def get_all_concerts(request: Request):
@@ -11,8 +12,15 @@ def get_all_concerts(request: Request):
     :return: A json object containing the concerts of all artists in the playlist.
     """
     playlist_id = request.args['playlist_id']
-    location = request.args['location']
-    pass
+    city = request.args['city']
+
+    # Should it return a list of json objects?
+    artists = playlist_data.get_artist_names(playlist_id)
+    concerts = []
+
+    for artist in artists:
+        concerts.append(concert_data.get_event_key_city(artist, city))
+    return concerts
 
 
 def get_concert(request: Request):
@@ -21,4 +29,5 @@ def get_concert(request: Request):
     :param request: flask.Request object.
     :return: Comma separated string of unique ids.
     """
+    concert_id = request.args['concert_id']
     pass
