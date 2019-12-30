7c0208405985a93b47e481c1e1c1347f962e0e35
Shannon Komguem <s.komguem@mail.utoronto.ca>
2019-12-30 00:39:08 -0500

    finished ticketmaster functions:

--- model/concert_data.py
+++ model/concert_data.py
@@ -1,19 +1,9 @@
 """
 All the ticketmaster stuff will be handled here.
 """
-import ticketpy, json
-from geopy.geocoders import Nominatim
-import ssl
-import certifi
-import geopy.geocoders
-
-# Initialize ticketmaster client
+import requests, json
+URL = 'http://app.ticketmaster.com//discovery/v2/events.json?'
 API_KEY = 'k2zkHLSGtq9fYAsPhysiijSpxn3GGkNh'
-TM_CLIENT = ticketpy.ApiClient(API_KEY)
-
-# Initialize geolocation
-ctx = ssl.create_default_context(cafile=certifi.where())
-geopy.geocoders.options.default_ssl_context = ctx
 
 
 def get_event_keyword(keyword: str) -> json:
@@ -22,27 +12,30 @@ def get_event_keyword(keyword: str) -> json:
     :param keyword: The keyword that will be search for in the events.
     :return: A json object containing the search results.
     """
-    pass
+    url = URL + "keyword={}&apikey={}".format(keyword, API_KEY)
+    return requests.get(url).json()
 
 
-def get_event_location(location: str) -> json:
+def get_event_city(city: str) -> json:
     """ Search for an event based on location.
 
-    :param location: The location that will be searched for.
+    :param city: The location that will be searched for.
     :return: A json object containing the search results.
     """
-    pass
+    url = URL + "city={}&apikey={}".format(city, API_KEY)
+    return requests.get(url).json()
 
 
-def get_event_key_loc(keyword: str, location: str) -> json:
+def get_event_key_city(keyword: str, city: str) -> json:
     """
 
     :param keyword: The keyword that will be search for in the events.
-    :param location: The location that will be searched for.
+    :param city: The location that will be searched for.
     :return: A json object containing the search results.
     """
-    pass
+    url = URL + "keyword={}&city={}&apikey={}".format(keyword, city, API_KEY)
+    return requests.get(url).json()
 
 
 if __name__ == '__main__':
-    pass
+    print(get_event_key_city('Dijon', 'Toronto'))
